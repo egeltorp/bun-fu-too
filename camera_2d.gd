33f3276c17c7bf7c5cd25d86b504ddc17b5d3cb7
egeltorp <35803316+egeltorp@users.noreply.github.com>
2025-10-27 04:50:36 +0100

    Add camera offset variables for improved player tracking

--- camera_2d.gd
+++ camera_2d.gd
@@ -1,5 +1,9 @@
 extends Camera2D
 
+# Camera Offset Values
+@export var pos_offset_y: float = -16.0 # Height, - up, + down
+@export var pos_offset_x: float = 8.0 # X-axis, - left, + right
+
 @export var target_path: NodePath  # Set this in the editor to point to your player
 @export var vertical_deadzone := 0  # Pixels above or below the camera center before it follows
 @onready var player = get_node(target_path)
@@ -24,10 +28,10 @@ func _process(delta):
 	var cam_pos = global_position
 
 	# Always follow on X
-	cam_pos.x = player_pos.x
+	cam_pos.x = player_pos.x + pos_offset_x
 
 	# Only follow on Y if player leaves the deadzone
-	var y_diff = player_pos.y - cam_pos.y
+	var y_diff = player_pos.y - cam_pos.y + pos_offset_y
 
 	if abs(y_diff) > vertical_deadzone:
 		# Move camera toward the player to bring them back within deadzone
@@ -35,6 +39,10 @@ func _process(delta):
 		cam_pos.y += (abs(y_diff) - vertical_deadzone) * direction
 
 	global_position = cam_pos
+	
+func reverse_x_offset():
+	# reverses the value of the offset, flips left/right
+	pos_offset_x *= -1
 
 func shake(strength: float = 5.0, decay: float = 5.0):
 	shake_strength = strength
